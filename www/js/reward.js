080a0a0c68c5f45d936a3bf3e49ee0622407fc63
Peter Vajda <vaphab@gmail.com>
2013-12-14 21:46:57 -0800

    load/save values for goal

--- www/js/reward.js
+++ www/js/reward.js
@@ -1,3 +1,45 @@
+var idGoal = 0;
+var maxIdGoal = 0;
+var goal = [];
+var nImages = 0;
+function saveGoal(){
+  if (maxIdGoal < idGoal) {
+    maxIdGoal = idGoal;
+    id = "goal_"+idGoal
+    $('#goalList').append('<li><a id="'+id+'">'+$('#goalName').val()+'</a></li>').listview('refresh');
+   $('#'+id).bind( "click", function() {
+        showGoal(idGoal)
+    });
+  }
+  goal[idGoal] = {
+    "name": $('#goalName').val(),
+    "description": $('#goalDescription').val(),
+    "date": $('#goalDate').val(),
+    "image1": $('#newPhoto').attr('src'),
+    "image2": $('#newPhoto2').attr('src'),
+  };
+  $.mobile.changePage('#StudentView');
+}
+
+function showGoal(idGoal){
+  $('#goalName').val(goal[idGoal]["name"]);
+  $('#goalDescription').val(goal[idGoal]["description"]);
+  $('#goalDate').val(goal[idGoal]["date"]);
+  $('#newPhoto').val(goal[idGoal]["image"]);
+  $('#newPhoto2').val(goal[idGoal]["image2"]);
+  $.mobile.changePage('#GoalView');
+}
+
+function addGoal(){
+  idGoal = maxIdGoal+1;
+  $.mobile.changePage('#GoalView');
+  nImages = 0;
+  $('#goalName').val("");
+  $('#goalDescription').val("");
+  $('#goalDate').val("");
+  $('#newPhoto').val("");
+  $('#newPhoto2').val("");
+}
 
 // Called when capture operation is finished
 //
@@ -17,10 +59,11 @@ function captureError(error) {
 function captureImage() {
   // Launch device camera application, 
   // Take picture using device camera and retrieve image as base64-encoded string
-  navigator.camera.getPicture(captureSuccess, captureError, { quality: 50, destinationType: Camera.DestinationType.DATA_URL});
+  //navigator.camera.getPicture(captureSuccess, captureError, { quality: 50, destinationType: Camera.DestinationType.DATA_URL});
 }
 
 function displayPhoto(imageData){
-   $('#newPhoto').attr('src',"data:image/jpeg;base64," + imageData);
+   $('#newPhoto'+nImages).attr('src',"data:image/jpeg;base64," + imageData);
+   nImages += 1;
 }
 
